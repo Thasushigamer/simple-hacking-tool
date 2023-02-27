3e869860fdc0fa3312befe77460586a2c50ed29d
Thasushigamer <102922862+Thasushigamer@users.noreply.github.com>
2023-02-27 18:54:57 +0100

    Add files via upload

---
+++ primohackingtool.py
@@ -0,0 +1,53 @@
+import os
+import webbrowser
+from platform import system
+from time import *
+import Pylance
+from core import HackingToolsCollection
+
+os.system("clear")
+
+Logo="""
+                                                                                                                            ______
+                                                                                                                         .-"      "-.	
+                                                                                                                        /            \	        
+ __    __       ___       ______  __  ___  _______ .______         .___________.  ______     ______    __              |              |
+|  |  |  |     /   \     /      ||  |/  / |   ____||   _  \        |           | /  __  \   /  __  \  |  |             |,  .-.  .-.  ,|
+|  |__|  |    /  ^  \   |  ,----'|  '  /  |  |__   |  |_)  |       `---|  |----`|  |  |  | |  |  |  | |  |             | )(__/  \__)( |
+|   __   |   /  /_\  \  |  |     |    <   |   __|  |      /            |  |     |  |  |  | |  |  |  | |  |             |/     /\     \|
+|  |  |  |  /  _____  \ |  `----.|  .  \  |  |____ |  |\  \----.       |  |     |  `--'  | |  `--'  | |  `----.        (_     ^^     _)
+|__|  |__| /__/     \__\ \______||__|\__\ |_______|| _| `._____|       |__|      \______/   \______/  |_______|         \__|IIIIII|__/
+                                 .:.:.      DO NOT USE FOR ILLEGAL PURPOSES       .:.:.                                     | \IIIIII/ |
+                                 .:.:.    https://github.com/Thasushigamer     .:.:.                                     \          /
+                                 .:.:.              Version BETA               .:.:.                                      `--------`
+
+"""                                                                                                                      
+print(Logo)
+
+print("""
+[1] TOOL Wifi crack /BETA/
+[2] TOOL Phishing /BETA/
+[3] Uptade
+[99] Exit
+""")
+option = int(input("────choose────⮞:"))
+
+if (option==1):
+	os.system("clear")
+	print(Logo)
+	print("""
+[1111]TOOL-X
+[2222]Devam E
+[9898]returnt
+""")
+ 
+option72=input("────scegli────⮞:")
+
+elif  (option72==1111):
+os.system("clear && $ git clone https://github.com/m4ll0k/Infoga.git && cd Infoga && python setup.py install && python infoga.py && exit")
+
+elif  (option72==9898):
+os.system("exit && python3 primohackingtool.py")
+
+elif  (option==99):
+os.system("exit")
